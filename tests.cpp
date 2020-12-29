ef55fccca71ef3763872ace658fd58c9a7a59a73
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-12-29 11:59:34 -0500

    Updated tests

--- tests.cpp
+++ tests.cpp
@@ -59,6 +59,33 @@ TEST(division, veryBigOperands) {
   polynomialDivision(dividend, divisor, quotient, remainder, COUNT);
   EXPECT_EQ(remainder[0], 0);
   EXPECT_EQ(remainder[1], 0);
+  EXPECT_EQ(remainder[2], 0);
+  EXPECT_EQ(remainder[3], 0);
+  EXPECT_EQ(quotient[0], 0b10);
+  EXPECT_EQ(quotient[1], 0);
+  EXPECT_EQ(quotient[2], 0);
+  EXPECT_EQ(quotient[3], 0);
+}
+
+TEST(division, veryBigOperandsRemainder) {
+  int COUNT = 4;
+  auto quotient = new unsigned int[COUNT];
+  auto remainder = new unsigned int[COUNT];
+  auto dividend = new unsigned int[COUNT];
+  dividend[0] = 0b11111111111111111111111111111111;
+  dividend[1] = 0b11111111111111111111111111111111;
+  dividend[2] = 0b11111111111111111111111111111111;
+  dividend[3] = 0b11111111111111111111111111111111;
+  auto divisor = new unsigned int[COUNT];
+  divisor[0] = 0b11111111111111111111111111111111;
+  divisor[1] = 0b11111111111111111111111111111111;
+  divisor[2] = 0b11111111111111111111111111111111;
+  divisor[3] = 0b01111111111111111111111111111111;
+  polynomialDivision(dividend, divisor, quotient, remainder, COUNT);
+  EXPECT_EQ(remainder[0], 0b1);
+  EXPECT_EQ(remainder[1], 0);
+  EXPECT_EQ(remainder[2], 0);
+  EXPECT_EQ(remainder[3], 0);
   EXPECT_EQ(quotient[0], 0b10);
   EXPECT_EQ(quotient[1], 0);
   EXPECT_EQ(quotient[2], 0);
